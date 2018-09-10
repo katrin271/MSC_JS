a42e69c9064f40deb59f5b564050665200f14e78
katrin271 <42409104+katrin271@users.noreply.github.com>
2018-09-11 00:10:06 +0300

    часть 4

---
+++ 4 часть/1/if_else.js
@@ -0,0 +1,6 @@
+let kkk=prompt("Введите число");
+if(kkk>=0) {
+	console.log(kkk);
+}else if (kkk<0) {
+	console.log(Math.abs("kkk"));	
+}
---
+++ 4 часть/1/js/if_else.js
@@ -0,0 +1,9 @@
+let kkk=prompt("Введите число");
+kkk=parseInt(kkk);
+if(kkk=0) {
+	console.log("0");
+}else if(kkk>=1) {
+	console.log(kkk);
+}else if (kkk<=-1) {
+	console.log(Math.abs("kkk"));	
+}
